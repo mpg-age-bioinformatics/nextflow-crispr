e5d8b113d59bd5a2c63dccd85f742cc0506c7f27
Jorge Boucas <jorgeboucas@users.noreply.github.com>
2025-01-07 10:04:39 +0100

    Update nextflow-crispr.sh

--- nextflow-crispr.sh
+++ nextflow-crispr.sh
@@ -142,7 +142,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S'): cleanR library formatting"
 nextflow run ${ORIGIN}nf-crisprcleanr ${CLEANR_RELEASE} -params-file ${PARAMS} -entry lib_file_formatting -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-cleanR-lib.log ${LOGS_}/nf-cleanR-lib.log && sleep 1
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): fastqc"
-nextflow run ${ORIGIN}nf-fastqc ${FASTQC_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-fastqc.log ${LOGS_}/nf-fastqc.log & FASTQC_PID=$!
+nextflow run ${ORIGIN}nf-fastqc ${FASTQC_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-fastqc.log ${LOGS_}/nf-fastqc.log && sleep 1 #& FASTQC_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): cutadapt"
 nextflow run ${ORIGIN}nf-cutadapt ${CUTADAPT_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-cutadapt.log ${LOGS_}/nf-cutadapt.log && sleep 1
@@ -160,51 +160,51 @@ echo "$(date '+%Y-%m-%d %H:%M:%S'): cleanR pipe"
 nextflow run ${ORIGIN}nf-crisprcleanr ${CLEANR_RELEASE} -params-file ${PARAMS} -entry cleanR_workflow -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-cleanR-pipe.log ${LOGS_}/nf-cleanR-pipe.log && sleep 1
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): magecku"
-nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry magecku -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-u.log ${LOGS_}/nf-mageck-u.log & MAGECK_U_PID=$!
+nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry magecku -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-u.log ${LOGS_}/nf-mageck-u.log && sleep 1 #& MAGECK_U_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): mageck pathway"
-nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_pathway -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-pathway.log ${LOGS_}/nf-mageck-pathway.log & MAGECK_PATHWAY_PID=$!
+nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_pathway -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-pathway.log ${LOGS_}/nf-mageck-pathway.log && sleep 1 #& MAGECK_PATHWAY_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): mageck plot"
-nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_plot -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-plot.log ${LOGS_}/nf-mageck-plot.log & MAGECK_PLOT_PID=$!
+nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_plot -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-plot.log ${LOGS_}/nf-mageck-plot.log && sleep 1 #& MAGECK_PLOT_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): bagel"
-nextflow run ${ORIGIN}nf-bagel ${BAGEL_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-bagel.log ${LOGS_}/nf-bagel.log & BAGEL_PID=$!
+nextflow run ${ORIGIN}nf-bagel ${BAGEL_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-bagel.log ${LOGS_}/nf-bagel.log && sleep 1 #& BAGEL_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): drugz"
-nextflow run ${ORIGIN}nf-drugz ${DRUGZ_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-drugz.log ${LOGS_}/nf-drugz.log & DRUGZ_PID=$!
+nextflow run ${ORIGIN}nf-drugz ${DRUGZ_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-drugz.log ${LOGS_}/nf-drugz.log && sleep 1 #& DRUGZ_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): acer"
-nextflow run ${ORIGIN}nf-acer ${ACER_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-acer.log ${LOGS_}/nf-acer.log & ACER_PID=$!
+nextflow run ${ORIGIN}nf-acer ${ACER_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-acer.log ${LOGS_}/nf-acer.log && sleep 1 #& ACER_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): maude"
-nextflow run ${ORIGIN}nf-maude ${MAUDE_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-maude.log ${LOGS_}/nf-maude.log & MAUDE_PID=$!
+nextflow run ${ORIGIN}nf-maude ${MAUDE_RELEASE} -params-file ${PARAMS} -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-maude.log ${LOGS_}/nf-maude.log && sleep 1 #& MAUDE_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): mageck mle"
 nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_premle -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-premle.log ${LOGS_}/nf-mageck-premle.log && \
 nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_mle -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-mle.log ${LOGS_}/nf-mageck-mle.log && sleep 1
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): mageck vispr"
-nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_vispr -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-vispr.log ${LOGS_}/nf-mageck-vispr.log & MAGECK_VISPR_PID=$!
+nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_vispr -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-vispr.log ${LOGS_}/nf-mageck-vispr.log && sleep 1 #& MAGECK_VISPR_PID=$!
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): mageck flute"
-nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_flute -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-flute.log ${LOGS_}/nf-mageck-flute.log & MAGECK_FLUTE_PID=$!
+nextflow run ${ORIGIN}nf-mageck ${MAGECK_RELEASE} -params-file ${PARAMS} -entry mageck_flute -profile ${PROFILE} 2>&1 | tee ${LOGS}/nf-mageck-flute.log ${LOGS_}/nf-mageck-flute.log && sleep 1 #& MAGECK_FLUTE_PID=$!
 
 
-for PID in "${FASTQC_PID}:FASTQC" \
-  "${FASTQC_CLEAN_PID}:FASTQC_CLEAN" \
-  "${MAGECK_U_PID}:MAGECK_U_PID" \
-  "${MAGECK_PATHWAY_PID}:MAGECK_PATHWAY" \
-  "${MAGECK_PLOT_PID}:MAGECK_PLOT" \
-  "${BAGEL_PID}:BAGEL" \
-  "${DRUGZ_PID}:DRUGZ" \
-  "${ACER_PID}:ACER" \
-  "${MAUDE_PID}:MAUDE" \
-  "${MAGECK_VISPR_PID}:MAGECK_VISPR" \
-  "${MAGECK_FLUTE_PID}:MAGECK_FLUTE"
-  do 
-    wait_for $PID
-done
+# for PID in "${FASTQC_PID}:FASTQC" \
+#   "${FASTQC_CLEAN_PID}:FASTQC_CLEAN" \
+#   "${MAGECK_U_PID}:MAGECK_U_PID" \
+#   "${MAGECK_PATHWAY_PID}:MAGECK_PATHWAY" \
+#   "${MAGECK_PLOT_PID}:MAGECK_PLOT" \
+#   "${BAGEL_PID}:BAGEL" \
+#   "${DRUGZ_PID}:DRUGZ" \
+#   "${ACER_PID}:ACER" \
+#   "${MAUDE_PID}:MAUDE" \
+#   "${MAGECK_VISPR_PID}:MAGECK_VISPR" \
+#   "${MAGECK_FLUTE_PID}:MAGECK_FLUTE"
+#   do 
+#     wait_for $PID
+# done
 
 rsync -rtvh ${LOGS} ${project_folder}
 
